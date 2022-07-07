5b1b572a59376c1ed4168b99b42fadc8c6161be7
Ian <ianfund117@gmail.com>
2022-07-07 17:22:04 -0500

    Added data map but can't view on laptop??

--- src/App.js
+++ src/App.js
@@ -3,14 +3,38 @@ import "./App.css";
 import data from "./data.js";
 import Header from "./Components/Header";
 import Card from "./Components/Card";
+console.log("Data are ",data);
+const entries = data.map(x => {
+  return(
+    <Card 
+        img={data.imageUrl}
+        location={data.location}
+        startDate={data.startDate}
+        endDate={data.endDate}
+        description={data.description}
 
+    />
+  )
+})
 function App() {
   return (
     <div className="App">
       <Header />
-      <Card />
+      {entries}
     </div>
   );
 }
 
 export default App;
+
+
+
+// {
+//   title: "Mount Fuji",
+//   location: "Japan",
+//   googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+//   startDate: "12 Jan, 2021",
+//   endDate: "24 Jan, 2021",
+//   description:
+//     "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+//   imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,36 @@
 import React from "react";
 import data from "../data";
 
-export default function Card() {
+export default function Card(props) {
   const locations = data.map((x) => {
     return <div>{x.location}</div>;
   });
-  return <div>{locations}</div>;
+  console.log("props are: ",props);
+  return (
+      <div className="card">
+          <img src={props.img} />
+          <div className="journal--entry">
+            {props.title}
+            {props.location}
+            {props.startDate}
+            {props.endDate}
+            {props.description}
+
+
+
+          </div>
+
+    </div>
+    );
 }
+
+
+// {
+//   title: "Mount Fuji",
+//   location: "Japan",
+//   googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+//   startDate: "12 Jan, 2021",
+//   endDate: "24 Jan, 2021",
+//   description:
+//     "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+//   imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
